705bb4fd8da52d2edfb025af6b080370de52d538
PeterTodorovv <petertodorovv@gmail.com>
2022-01-29 21:30:06 +0200

    01/29

---
+++ 01,Inheritance/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings: Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count < 1;
+        }
+
+        public void AddRange(IEnumerable<string> stack)
+        {
+            foreach(var element in stack)
+            {
+                this.Push(element);
+            }
+        }
+    }
+}
