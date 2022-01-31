56bbc5b5952d12d69bee7dec23abbf46f71d144f
PeterTodorovv <petertodorovv@gmail.com>
2022-02-01 00:15:12 +0200

    02/01

---
+++ 01,Inheritance/01.Person/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Person
+{
+    class Person
+    {
+        public Person()
+        {
+
+        }
+
+        public int Age;
+    }
+}
