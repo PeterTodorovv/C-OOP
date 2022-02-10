774e0c9abba5965697b0a5abbe72af2705cee68e
PeterTodorovv <petertodorovv@gmail.com>
2022-02-10 13:20:45 +0200

    02/10

---
+++ 02.Encapsulation/05.FootballTeamGenerator/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.FootballTeamGenerator
+{
+    class Player
+    {
+        private string playerName;
+        private int endurance;
+        private int sprint;
+        private int dribble;
+        private int passing;
+        private int shooting;
+
+        public Player(string playerName, int endurance, int sprint, int dribble, int passing, int shooting)
+        {
+            PlayerName = playerName;
+            Endurance = endurance;
+            Sprint = sprint;
+            Dribble = dribble;
+            Passing = passing;
+            Shooting = shooting;
+        }
+
+        public string PlayerName
+        {
+            get { return playerName; }
+            set { playerName = value; }
+        }
+        public int Endurance
+        {
+            get { return endurance; }
+            set { endurance = value; }
+        }
+        public int Sprint
+        {
+            get { return sprint; }
+            set { sprint = value; }
+        }
+        public int Dribble
+        {
+            get { return dribble; }
+            set { dribble = value; }
+        }
+        public int Passing
+        {
+            get { return passing; }
+            set { passing = value; }
+        }
+        public int Shooting
+        {
+            get { return shooting; }
+            set { shooting = value; }
+        }
+
+        public int SkillLevel { get => (Endurance + Sprint + Dribble + Passing + Shooting) / 5; }
+
+    }
+}
---
+++ 02.Encapsulation/05.FootballTeamGenerator/Team.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05.FootballTeamGenerator
+{
+    class Team
+    {
+        private string name;
+
+        public Team(string name)
+        {
+            Name = name;
+            players = new List<Player>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private List<Player> players;
+        public int Rating { get => (int)Math.Round(players.Average(p => p.SkillLevel)); }
+
+        public void AddPlayer(Player player)
+        {
+            players.Add(player);
+        }
+        
+        public void RemovePlayer(string playerName)
+        {
+            players.Remove(players.FirstOrDefault(p => p.PlayerName == playerName));
+        }
+    }
+}
