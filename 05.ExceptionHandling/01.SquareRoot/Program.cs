0830a63ab791d1df9b5d4c1c55f6f3702e976996
PeterTodorovv <petertodorovv@gmail.com>
2022-02-26 22:29:48 +0200

    02/26

---
+++ 05.ExceptionHandling/01.SquareRoot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < 0)
+                    throw new ArgumentException();
+                Console.WriteLine(Math.Sqrt(number));
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("InvalidNumber");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+        }
+    }
+}
