e197fa5766589a6e9f9ad91518b8aafa38cdf7dc
PeterTodorovv <petertodorovv@gmail.com>
2022-03-12 16:26:18 +0200

    03/12

---
+++ 08.UnitTesting/01.TextAge/Skeleton.Tests/AxeTests.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void AxeShouldLooseDurabilityAfterAtack()
+        {
+            Axe axe = new Axe(1, 10);
+            Dummy dummy = new Dummy(100, 10);
+            axe.Attack(dummy);
+            Assert.AreEqual(axe.DurabilityPoints, 9);
+        }
+
+        [Test]
+        public void AxeCantAttackIfBroken()
+        {
+            Axe axe = new Axe(1, 0);
+            Dummy dummy = new Dummy(100, 10);
+
+            Assert.That(() => axe.Attack(dummy), Throws.Exception);
+        }
+    }
+}
