a072d7aa52dd1d7bc5e4a19d7abbc17cca9aad23
PeterTodorovv <petertodorovv@gmail.com>
2022-03-20 15:10:14 +0200

    03/20

--- 08.UnitTesting/Exercies/FightingArena.Tests/ArenaTests.cs
+++ 08.UnitTesting/Exercies/FightingArena.Tests/ArenaTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Tests
 {
@@ -10,9 +11,53 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void DifferentWarriorsCanBeEnroled()
         {
-            Assert.Pass();
+            Arena arena = new Arena();
+            Warrior warrior = new Warrior("Ivan", 30, 100);
+            Warrior warrior2 = new Warrior("Gosho", 301, 90);
+            Warrior warrior3 = new Warrior("Pesho", 69, 420);
+
+            arena.Enroll(warrior);
+            arena.Enroll(warrior2);
+            arena.Enroll(warrior3);
+
+            Assert.AreEqual(3, arena.Count);
+
+        }
+
+        [Test]
+        public void CantEnrollTheSameWarriorTwise()
+        {
+            Arena arena = new Arena();
+            Warrior warrior = new Warrior("Ivan", 30, 100);
+            arena.Enroll(warrior);
+            Assert.Throws<InvalidOperationException>(() => arena.Enroll(warrior));
+        }
+
+        [Test]
+        public void IfTheTwoWarriorsAreOnTheArenaTheyCanFight()
+        {
+            Warrior warrior = new Warrior("Ivan", 30, 100);
+            Warrior warrior2 = new Warrior("Gosho", 301, 90);
+
+            Arena arena = new Arena();
+            arena.Enroll(warrior);
+            arena.Enroll(warrior2);
+
+            Assert.DoesNotThrow(() => arena.Fight("Gosho", "Ivan"));
+        }
+
+        [Test]
+        public void ThereCanNotBeAfightIfBothWarriorsArentEnrolled()
+        {
+            Warrior warrior = new Warrior("Ivan", 30, 100);
+            Arena arena = new Arena();
+            arena.Enroll(warrior);
+
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("Gosho", "Ivan"));
+            Assert.Throws<InvalidOperationException>(() => arena.Fight("Ivan", "Gosho"));
+
         }
     }
 }
--- 08.UnitTesting/Exercies/FightingArena.Tests/WarriorTests.cs
+++ 08.UnitTesting/Exercies/FightingArena.Tests/WarriorTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 
 namespace Tests
 {
@@ -10,9 +11,87 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        [TestCase("James", 20, 50)]
+        [TestCase("Ivan", 1, 501)]
+        [TestCase("Gosho", 100, 3)]
+        public void ConstructorShouldWorkIfDataIsValid(string name, int dmg, int hp)
         {
-            Assert.Pass();
+            Warrior warrior = new Warrior(name, dmg, hp);
+
+            Assert.AreEqual(name, warrior.Name);
+            Assert.AreEqual(dmg, warrior.Damage);
+            Assert.AreEqual(hp, warrior.HP);
+
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void NameCannotBeNullEmptyOrWhitespace(string name)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior(name, 20 , 30));
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-123)]
+        public void DamageCantBeZeroOrNegavite(int dmg)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Jake", dmg, 30));
+
+        }
+
+        [TestCase(-123)]
+        [TestCase(-23)]
+        public void HpCantBeNegative(int hp)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Jake", 13, hp));
+
+        }
+
+        [Test]
+        [TestCase(10)]
+        [TestCase(1)]
+        [TestCase(29)]
+        public void WarriorCantAttackIfHeIsUnder30Hp(int hp)
+        {
+            Warrior attackWarrior = new Warrior("Jake", 20, hp);
+            Warrior defenceWarrior = new Warrior("Ivan", 10, 50);
+            Assert.Throws<InvalidOperationException>(() => attackWarrior.Attack(defenceWarrior));
+        }
+
+        [Test]
+        [TestCase(10)]
+        [TestCase(1)]
+        [TestCase(29)]
+        public void WarriorCantAttackEnemiesUnder30Hp(int hp)
+        {
+            Warrior attackWarrior = new Warrior("Jake", 20, 40);
+            Warrior defenceWarrior = new Warrior("Ivan", 10, hp);
+            Assert.Throws<InvalidOperationException>(() => attackWarrior.Attack(defenceWarrior));
+        }
+
+        [Test]
+        [TestCase(50)]
+        [TestCase(41)]
+
+        public void WarriorCantAttackStrongerEnemy(int dmg)
+        {
+            Warrior attackWarrior = new Warrior("Jake", 20, 40);
+            Warrior defenceWarrior = new Warrior("Ivan", dmg, 100);
+            Assert.Throws<InvalidOperationException>(() => attackWarrior.Attack(defenceWarrior));
+        }
+
+        [TestCase(110, 10)]
+        public void DamageIsCalculatedProperly(int atackerDmg, int defenceDmg)
+        {
+            Warrior attackWarrior = new Warrior("Jake", atackerDmg, 40);
+            Warrior defenceWarrior = new Warrior("Ivan", defenceDmg, 100);
+
+            attackWarrior.Attack(defenceWarrior);
+            Assert.AreEqual(0, defenceWarrior.HP);
+            Assert.AreEqual(30, attackWarrior.HP);
         }
     }
 }
