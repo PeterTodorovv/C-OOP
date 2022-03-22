dd2e9fae092d84139dfabb3105d717db5384fc3c
PeterTodorovv <petertodorovv@gmail.com>
2022-03-22 19:25:12 +0200

    03/22

---
+++ ExamPreparation/Car/CarRacing/Models/Cars/Car.cs
@@ -0,0 +1,109 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Cars
+{
+    public abstract class Car : ICar
+    {
+        private string make;
+        private string model;
+        private string vIN;
+        private int horsePower;
+        private double fuelAvailable;
+        private double fuelConsumptionPerRace;
+
+        protected Car(string make, string model, string vIN, int horsePower, double fuelAvailable, double fuelConsumptionPerRace)
+        {
+            Make = make;
+            Model = model;
+            VIN = vIN;
+            HorsePower = horsePower;
+            FuelAvailable = fuelAvailable;
+            FuelConsumptionPerRace = fuelConsumptionPerRace;
+        }
+
+        public string Make
+        {
+            get => make; set
+            {
+                if (value == null || value == " ")
+                {
+                    throw new ArgumentNullException("Car make cannot be null or empty.");
+                }
+                else
+                {
+                    make = value;
+                }
+            }
+        }
+        public string Model
+        {
+            get => model; set
+            {
+                if (value == null || value == " ")
+                {
+                    throw new ArgumentNullException("Car model cannot be null or empty.");
+                }
+                else
+                {
+                    model = value;
+                }
+            }
+        }
+
+        public string VIN
+        {
+            get => vIN; set
+            {
+                if (value.Length != 17)
+                {
+                    throw new ArgumentException("Car VIN must be exactly 17 characters long.");
+                }
+            }
+        }
+
+        public int HorsePower
+        {
+            get => horsePower; set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Horse power cannot be below 0.");
+                }
+                else
+                {
+                    horsePower = value;
+                }
+            }
+        }
+
+        public double FuelAvailable { get => fuelAvailable; set 
+            {
+                if(value < 0)
+                {
+                    fuelAvailable = 0;
+                }
+                else
+                {
+                    fuelAvailable = value;
+                }
+            }
+        }
+
+        public double FuelConsumptionPerRace { get => fuelConsumptionPerRace; set 
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("Fuel consumption cannot be below 0.");
+                }
+            }
+        }
+
+        public virtual void Drive()
+        {
+            FuelAvailable -= FuelConsumptionPerRace;
+        }
+    }
+}
---
+++ ExamPreparation/Car/CarRacing/Models/Racers/Racer.cs
@@ -0,0 +1,84 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using CarRacing.Models.Racers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Racers
+{
+    public abstract class Racer : IRacer
+    {
+        private string username;
+        private string racingBehavior;
+        private int drivingExperience;
+        private ICar car;
+
+        protected Racer(string username, string racingBehavior, int drivingExperience, ICar car)
+        {
+        }
+
+        public string Username { get => username; set
+            {
+                if (value == null || value == " ")
+                {
+                    throw new ArgumentNullException("Username cannot be null or empty.");
+                }
+                else
+                {
+                    username = value;
+                }
+            }
+        }
+
+        public string RacingBehavior
+        {
+            get => racingBehavior; set
+            {
+                if (value == null || value == " ")
+                {
+                    throw new ArgumentNullException("Racing behavior cannot be null or empty.");
+                }
+                else
+                {
+                    racingBehavior = value;
+                }
+            }
+        }
+
+        public int DrivingExperience { get => drivingExperience; set 
+            {
+                if(value < 0 || value > 100)
+                {
+                    throw new ArgumentException("Racer driving experience must be between 0 and 100.");
+                }
+                else
+                {
+                    drivingExperience = value;
+                }
+            }
+        }
+
+        public ICar Car { get => car; set 
+            {
+                if(value == null)
+                {
+                    throw new ArgumentException("Car cannot be null or empty.");
+                }
+                else
+                {
+                    car = value;
+                }
+            }
+        }
+
+        public bool IsAvailable()
+        {
+            return car.FuelAvailable >= car.FuelConsumptionPerRace;
+        }
+
+        public virtual void Race()
+        {
+            car.Drive();
+        }
+    }
+}
