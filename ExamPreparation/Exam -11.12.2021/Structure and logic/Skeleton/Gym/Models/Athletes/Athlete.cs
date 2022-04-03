33c82bff403c7108e015212957e40380cb2788f6
PeterTodorovv <petertodorovv@gmail.com>
2022-04-03 23:08:07 +0300

    04/02

--- ExamPreparation/Exam -11.12.2021/Structure and logic/Skeleton/Gym/Models/Athletes/Athlete.cs
+++ ExamPreparation/Exam -11.12.2021/Structure and logic/Skeleton/Gym/Models/Athletes/Athlete.cs
@@ -1,33 +1,25 @@
 ﻿using Gym.Models.Athletes.Contracts;
+using Gym.Utilities.Messages;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Linq;
 
 namespace Gym.Models.Athletes
 {
-    public abstract class Athlete : IAthlete
+    internal abstract class Athlete : IAthlete
     {
         private string fullName;
         private string motivation;
         private int numberOfMedals;
 
-        public Athlete(string fullName, string motivation, int numberOfMedals, int stamina)
-        {
-            FullName = fullName;
-            Motivation = motivation;
-            NumberOfMedals = numberOfMedals;
-            Stamina = stamina;
-        }
         public string FullName
         {
             get => fullName; private set
             {
                 if (string.IsNullOrEmpty(value))
                 {
-                    throw new ArgumentException("Athlete name cannot be null or empty.");
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteName);
                 }
-
                 fullName = value;
             }
         }
@@ -38,32 +30,24 @@ namespace Gym.Models.Athletes
             {
                 if (string.IsNullOrEmpty(value))
                 {
-                    throw new ArgumentException("The motivation cannot be null or empty.");
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteMotivation);
                 }
-
                 motivation = value;
             }
         }
 
-        public int Stamina { get; set; }
+        public int Stamina { get; private set; }
 
         public int NumberOfMedals { get => numberOfMedals; private set 
             {
-                if(value < 0)
+                if(numberOfMedals < 0)
                 {
-                    throw new ArgumentException("Athlete's number of medals cannot be below 0.");
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteMedals);
                 }
-
                 numberOfMedals = value;
             }
         }
 
-        public virtual void Exercise()
-        {
-            if (Stamina > 100)
-            {
-                throw new ArgumentException("Stamina cannot exceed 100 points.");
-            }
-        }
+        public abstract void Exercise();
     }
 }
